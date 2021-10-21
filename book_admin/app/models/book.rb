0675e32eb422ebe90b8f71975bb30f78091ff97c
shibayu36 <shibayu36@gmail.com>
2021-10-21 15:49:32 +0900

    Add validation for book

--- book_admin/app/models/book.rb
+++ book_admin/app/models/book.rb
@@ -3,6 +3,10 @@ class Book < ApplicationRecord
   has_many :book_authors
   has_many :authors, through: :book_authors
 
+  validates :name, presence: true
+  validates :name, length: { maximum: 25 }
+  validates :price, numericality: { greater_than_or_equal_to: 0 }
+
   scope :costly, -> { where('price > ?', 3000) }
   scope :written_about, ->(theme) { where('name like ?', "%#{theme}%") }
   scope :find_price, ->(price) { find_by(price: price) }
