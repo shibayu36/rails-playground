5b61b826cf01cbd9d63f9e85cb64e19728e5140a
shibayu36 <shibayu36@gmail.com>
2021-10-21 15:04:39 +0900

    Add references between Book and Publisher

--- book_admin/app/models/book.rb
+++ book_admin/app/models/book.rb
@@ -1,4 +1,6 @@
 class Book < ApplicationRecord
+  belongs_to :publisher
+
   scope :costly, -> { where('price > ?', 3000) }
   scope :written_about, ->(theme) { where('name like ?', "%#{theme}%") }
   scope :find_price, ->(price) { find_by(price: price) }
