b136a4cb27c2a43a5aa84f471043a78d7bfe619b
shibayu36 <shibayu36@gmail.com>
2021-10-22 22:43:38 +0900

    bin/rails g controller Profiles show edit update

--- book_admin/config/routes.rb
+++ book_admin/config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  get 'profiles/show'
+  get 'profiles/edit'
+  get 'profiles/update'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   get '/books/:id' => 'books#show'
   delete '/books/:id' => 'books#destroy'
