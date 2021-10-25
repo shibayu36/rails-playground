aaa869e00f0a7a5fc176c88c3ba1d33f5a351d81
shibayu36 <shibayu36@gmail.com>
2021-10-25 22:33:47 +0900

    Add retry

--- active_job_example/app/jobs/async_log_job.rb
+++ active_job_example/app/jobs/async_log_job.rb
@@ -1,5 +1,6 @@
 class AsyncLogJob < ApplicationJob
   queue_as :async_log
+  retry_on StandardError, wait: 5.seconds, attempts: 3
 
   def perform(message: 'hello')
     AsyncLog.create!(message: message)
