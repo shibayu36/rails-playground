d9243ca015273acf3b8c464234e2dda458f5fb35
shibayu36 <shibayu36@gmail.com>
2021-10-25 22:14:08 +0900

    Implement AsyncLogJob

--- active_job_example/app/jobs/async_log_job.rb
+++ active_job_example/app/jobs/async_log_job.rb
@@ -1,7 +1,7 @@
 class AsyncLogJob < ApplicationJob
   queue_as :default
 
-  def perform(*args)
-    # Do something later
+  def perform(message: 'hello')
+    AsyncLog.create!(message: message)
   end
 end
