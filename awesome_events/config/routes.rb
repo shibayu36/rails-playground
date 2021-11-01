435fecde22ad15d487e25d751c553edebd8cd23a
shibayu36 <shibayu36@gmail.com>
2021-11-01 09:55:03 +0900

    Add a button to participate an event

--- awesome_events/config/routes.rb
+++ awesome_events/config/routes.rb
@@ -3,5 +3,9 @@ Rails.application.routes.draw do
   root 'welcome#index'
   get '/auth/:provider/callback' => 'sessions#create'
   delete '/logout' => 'sessions#destroy'
+
+  resources :events do
+    resources :tickets
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
