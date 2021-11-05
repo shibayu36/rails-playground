ff9e768cc7ae2bbe538ab1c22cd02ae97b2974eb
shibayu36 <shibayu36@gmail.com>
2021-11-05 23:19:00 +0900

    Implement login

--- auth0-on-rails/config/routes.rb
+++ auth0-on-rails/config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root 'root#index'
+  get '/auth/auth0/callback' => 'auth0#callback'
+  get '/auth/failure' => 'auth0#failure'
+  get '/auth/logout' => 'auth0#logout'
 end
