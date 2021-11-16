1224148aa5d90b5141faf8c911e530aa3c5cd7e7
shibayu36 <shibayu36@gmail.com>
2021-11-16 22:49:04 +0900

    Add minimum spec

---
+++ rspec-tutorial/spec/lib/bowling_spec.rb
@@ -0,0 +1,13 @@
+require 'bowling'
+
+RSpec.describe Bowling do
+  describe '#score' do
+    context 'with no strikes or spares' do
+      it 'sums the pin count for each roll' do
+        bowling = Bowling.new
+        20.times { bowling.hit(4) }
+        expect(bowling.score).to eq 80
+      end
+    end
+  end
+end
