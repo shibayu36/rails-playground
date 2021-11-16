269130d26224197d6d4a612a636238647c7f0458
shibayu36 <shibayu36@gmail.com>
2021-11-16 09:36:21 +0900

    Setup generator for test framework

--- rspec-tutorial/config/application.rb
+++ rspec-tutorial/config/application.rb
@@ -1,6 +1,6 @@
-require_relative "boot"
+require_relative 'boot'
 
-require "rails/all"
+require 'rails/all'
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
@@ -10,6 +10,14 @@ module RspecTutorial
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.1
+
+    config.generators do |g|
+      g.test_framework :rspec,
+                       fixtures: false,
+                       view_specs: false,
+                       helper_specs: false,
+                       routing_specs: false
+    end
 
     # Configuration for the application, engines, and railties goes here.
     #
