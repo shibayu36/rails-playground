df89a1afe1cb935902514afd39366afef18cc78d
shibayu36 <shibayu36@gmail.com>
2021-11-21 07:01:51 +0900

    request.variant trial

--- awesome_events/app/controllers/constraints_controller.rb
+++ awesome_events/app/controllers/constraints_controller.rb
@@ -1,7 +1,12 @@
 class ConstraintsController < ApplicationController
   skip_before_action :authenticate
 
+  layout 'constraints/layouts/constraints'
+
   def hoge
+    p(request.mobile?)
+    request.variant = :sp if request.smart_phone?
+
     p(domain1_constraints_hoge_path) # "/constraints/hoge"
     p(domain1_constraints_hoge_url) # "http://domain1.shibayu36.localhost:3000/constraints/hoge"
     p(domain2_constraints_hoge_path) # "/constraints/hoge"
