1ec7782eab3ea566532668e1107fc81579f92faa
shibayu36 <shibayu36@gmail.com>
2021-11-21 06:41:49 +0900

    Test constraints

---
+++ awesome_events/app/controllers/constraints_controller.rb
@@ -0,0 +1,21 @@
+class ConstraintsController < ApplicationController
+  skip_before_action :authenticate
+
+  def hoge
+    p(domain1_constraints_hoge_path) # "/constraints/hoge"
+    p(domain1_constraints_hoge_url) # "http://domain1.shibayu36.localhost:3000/constraints/hoge"
+    p(domain2_constraints_hoge_path) # "/constraints/hoge"
+    p(domain2_constraints_hoge_url) # "http://domain2.shibayu36.localhost:3000/constraints/hoge"
+    p(constraints_fuga_path) # "/constraints/fuga"
+    p(constraints_fuga_url) # "http://domain1.shibayu36.localhost:3000/constraints/fuga"
+  end
+
+  def fuga
+    # redirect_to domain1_constraints_hoge_path # pathだけ変わる。ドメインはそのまま
+    redirect_to domain1_constraints_hoge_url # domainも変わる
+  end
+
+  def piyo
+    head :ok
+  end
+end
--- awesome_events/config/routes.rb
+++ awesome_events/config/routes.rb
@@ -7,5 +7,26 @@ Rails.application.routes.draw do
   resources :events do
     resources :tickets
   end
+
+  constraints(host: 'domain1.shibayu36.localhost') do
+    get '/constraints/hoge', to: 'constraints#hoge', as: 'domain1_constraints_hoge'
+  end
+
+  constraints(host: 'domain2.shibayu36.localhost') do
+    get '/constraints/hoge', to: 'constraints#hoge', as: 'domain2_constraints_hoge'
+  end
+
+  constraints(host: ['domain1.shibayu36.localhost', 'domain2.shibayu36.localhost']) do
+    get '/constraints/fuga', to: 'constraints#fuga', as: 'constraints_fuga'
+  end
+
+  # 以下のような書き方はできない
+  # constraints(host: 'domain1.shibayu36.localhost') do
+  #   get '/constraints/piyo', to: 'constraints#piyo', as: 'constraints_piyo'
+  # end
+  # constraints(host: 'domain2.shibayu36.localhost') do
+  #   get '/constraints/piyo', to: 'constraints#piyo', as: 'constraints_piyo'
+  # end
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
