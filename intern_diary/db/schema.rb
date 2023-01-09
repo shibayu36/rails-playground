ec6954f3b45dc3ec2613d956190821b1370de665
shibayu36 <shibayu36@gmail.com>
2023-01-09 14:10:23 +0900

    Execute "bin/rails generate model User name:string:uniq" and "bin/rails db:migrate"

--- intern_diary/db/schema.rb
+++ intern_diary/db/schema.rb
@@ -10,5 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 0) do
+ActiveRecord::Schema[7.0].define(version: 2023_01_09_050709) do
+  create_table "users", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["name"], name: "index_users_on_name", unique: true
+  end
+
 end
