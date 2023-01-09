f00839f2ef9fa3bf7c8ff946dce8ccfc82f38bb6
shibayu36 <shibayu36@gmail.com>
2023-01-09 14:20:43 +0900

    Add NOT NULL constraints to users.name

--- intern_diary/db/schema.rb
+++ intern_diary/db/schema.rb
@@ -10,9 +10,9 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_01_09_050709) do
+ActiveRecord::Schema[7.0].define(version: 2023_01_09_051824) do
   create_table "users", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
-    t.string "name"
+    t.string "name", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["name"], name: "index_users_on_name", unique: true
