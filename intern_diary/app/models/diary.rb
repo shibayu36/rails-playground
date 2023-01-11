4c2c7bc8f992a9c006bf10736e4d8f083f40a823
shibayu36 <shibayu36@gmail.com>
2023-01-11 20:15:44 +0900

    Fix Dependent offenses

--- intern_diary/app/models/diary.rb
+++ intern_diary/app/models/diary.rb
@@ -2,5 +2,5 @@
 
 class Diary < ApplicationRecord
   belongs_to :user
-  has_many :entries
+  has_many :entries, dependent: :destroy
 end
--- intern_diary/app/models/user.rb
+++ intern_diary/app/models/user.rb
@@ -1,5 +1,5 @@
 # frozen_string_literal: true
 
 class User < ApplicationRecord
-  has_one :diary
+  has_one :diary, dependent: :destroy
 end
