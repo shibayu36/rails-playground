b0df81a99652689192ba42d8a2ed074edd87a95f
shibayu36 <shibayu36@gmail.com>
2023-01-14 22:54:37 +0900

    Implement diary.rb add command

--- intern_diary/diary.rb
+++ intern_diary/diary.rb
@@ -2,8 +2,10 @@
 
 case ARGV[0]
 in 'add'
-  puts 'add command'
-  puts ARGV[1..].join(' ')
+  username = ARGV[1] || abort('username is required')
+  title = ARGV[2] || abort('title is required')
+  body = ARGV[3] || abort('body is required')
+  AddCommand.exec(username, title, body)
 in 'delete'
   puts 'delete command'
   puts ARGV[1..].join(' ')
