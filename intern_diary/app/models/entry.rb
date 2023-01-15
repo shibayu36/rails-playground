ef49f3db7c0595ee8fd27556d06448c6dd984a1b
shibayu36 <shibayu36@gmail.com>
2023-01-15 17:15:57 +0900

    Implement the method making texts for console

--- intern_diary/app/models/entry.rb
+++ intern_diary/app/models/entry.rb
@@ -4,4 +4,11 @@ class Entry < ApplicationRecord
   belongs_to :diary
 
   scope :recent_ordered, -> { order(id: :desc) }
+
+  def to_terminal_output
+    <<~EOS
+      [#{id}] #{title} @#{created_at.strftime('%Y-%m-%d')}
+      #{body}
+    EOS
+  end
 end
--- intern_diary/spec/models/entry_spec.rb
+++ intern_diary/spec/models/entry_spec.rb
@@ -3,5 +3,22 @@
 require 'rails_helper'
 
 RSpec.describe Entry, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe '#to_terminal_output' do
+    let(:entry) do
+      build(
+        :entry,
+        title: 'title1',
+        body: "body1\nbody2",
+        created_at: Time.zone.local(2022, 3, 8)
+      )
+    end
+
+    it 'returns terminal output' do
+      expect(entry.to_terminal_output).to eq <<~EOS
+        [#{entry.id}] title1 @2022-03-08
+        body1
+        body2
+      EOS
+    end
+  end
 end
