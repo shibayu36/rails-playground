911d9bb08082c36237c5b6b7229fcc2c7b25b3e3
shibayu36 <shibayu36@gmail.com>
2023-01-16 19:40:31 +0900

    Implement validation of a diary and a entry

--- intern_diary/app/models/diary.rb
+++ intern_diary/app/models/diary.rb
@@ -3,4 +3,6 @@
 class Diary < ApplicationRecord
   belongs_to :user
   has_many :entries, dependent: :destroy
+
+  validates :name, presence: true, length: { maximum: 100 }
 end
--- intern_diary/app/models/entry.rb
+++ intern_diary/app/models/entry.rb
@@ -5,6 +5,9 @@ class Entry < ApplicationRecord
 
   scope :recent_ordered, -> { order(id: :desc) }
 
+  validates :title, length: { maximum: 100 }
+  validates :body, length: { maximum: 60_000 }
+
   def to_terminal_output
     <<~EOS
       [#{id}] #{title} @#{created_at.strftime('%Y-%m-%d')}
